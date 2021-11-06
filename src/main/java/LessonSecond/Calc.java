4843f7222ae3c507174948e4247c8761978b8f4a
vr <oazisone@mail.ru>
2021-11-06 16:22:20 +0300

    First

--- src/main/java/LessonSecond/Calc.java
+++ src/main/java/LessonSecond/Calc.java
@@ -1,4 +1,22 @@
 package LessonSecond;
 
 public class Calc {
+    public static void main(String[] args) {
+        System.out.println(plus(2,6));
+        System.out.println(minus(2,6));
+        System.out.println(division(2,6));
+        System.out.println(multiplication(2,6));
+    }
+    public static int plus(int b, int c){
+        return b + c;
+    }
+    public static int minus(int b, int c){
+        return b - c;
+    }
+    public static double division(double b, double c){
+        return b / c;
+    }
+    public static int multiplication(int b, int c){
+        return b * c;
+    }
 }
