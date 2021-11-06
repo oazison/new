7b67bd3d9af09ab7c0be43aab562e3b8a56616fd
vr <oazisone@mail.ru>
2021-11-06 17:29:57 +0300

    First

--- src/main/java/LessonSecond/Calc.java
+++ src/main/java/LessonSecond/Calc.java
@@ -6,6 +6,9 @@ public class Calc {
         System.out.println(minus(2,6));
         System.out.println(division(2,6));
         System.out.println(multiplication(2,6));
+        System.out.println("Ваше ускорение "  +acceleration(20,60,10) + " м/с");
+        System.out.println("Период колебания маятника "  +hesitation(20,60));
+
     }
     public static int plus(int b, int c){
         return b + c;
@@ -18,5 +21,27 @@ public class Calc {
     }
     public static int multiplication(int b, int c){
         return b * c;
+    }
+
+    /**
+     * Ускорение при равноускоренном прямолинейном движение.
+     * @param speedOne Начальная скорость.
+     * @param speedTwo Конечная скорость.
+     * @param time Время.
+     * @return Ускорение.
+     */
+    public static double acceleration(double speedOne, double speedTwo, double time){
+        return (speedTwo - speedOne)/time;
+    }
+
+    /**
+     * Период собственного колебания пружинного маятника
+     * @param massa Масса пружинного маятника.
+     * @param koef Коэфициент жесткости пружинного маятника.
+     * @return Период собственного колебания.
+     */
+    public static double hesitation (double massa, double koef){
+        double pi = 3.14;
+        return (2 * pi)*(Math.sqrt(massa/koef));
     }
 }
