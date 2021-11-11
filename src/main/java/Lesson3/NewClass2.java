aaef2d33e55dd2e21015508c128fc799d64cba6e
vr <oazisone@mail.ru>
2021-11-11 20:00:49 +0300

    First

--- src/main/java/Lesson3/NewClass2.java
+++ src/main/java/Lesson3/NewClass2.java
@@ -1,4 +1,15 @@
 package Lesson3;
 
 public class NewClass2 {
+    public static void main(String[] args) {
+        System.out.println(factorial(3));
+    }
+
+    public static int factorial(int a){
+        int b =1;
+        while (a > 0){
+            b = b * a--;
+        }
+        return b;
+    }
 }
