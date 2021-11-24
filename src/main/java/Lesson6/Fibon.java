de134a564a914f8d20600d1d649aa54d1dba98d0
oazison <oazisone@mail.ru>
2021-11-24 23:01:03 +0300

    Новый комит

--- src/main/java/Lesson6/Fibon.java
+++ src/main/java/Lesson6/Fibon.java
@@ -8,23 +8,42 @@ public class Fibon {
     public static void main(String[] args) {
         int[] array = randomLengthMassive();
         Scanner index = new Scanner(System.in);
-        int b = index.nextInt();
+        System.out.println("Введите число: ");
+        int in = index.nextInt();
+
+        if (in <= array.length) {
+            System.out.println("Число Фибоначчи по индексу " + in + " равно " + array[in] + ".");
+        } else {
+            int[] arr = new int[in];
+            int b = 0;
+            int e = 1;
+            for (int i = 0; i < in; i++) {
+                int f = b + e;
+                arr[i] = f;
+                b = e;
+                e = f;
+            }
+            for (int i : array = arr) {
+            }
+
+            System.out.println("Кеш обновлен.Число Фибоначчи по индексу " + in + " равно " + array[in - 1] + ".");
+            System.out.println(Arrays.toString(array));
+        }
     }
 
     public static int[] randomLengthMassive() {
         double randomDouble = Math.random() * 10;
-        int random = (int) randomDouble;
+        int random = (int) randomDouble * 2;
         int[] mass = new int[random];
         int b = 0;
         int e = 1;
-        for (int i = 0; i< random;i++ ){
+        for (int i = 0; i < random; i++) {
             int f = b + e;
-            mass [i] = f;
+            mass[i] = f;
             b = e;
             e = f;
         }
         return mass;
     }
-
 
 }
